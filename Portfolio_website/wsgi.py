4e41f79d2b151650ca2f7043be3c8d63c7a545fe
Suraj kumar <surajkumarsksk2000@gmail.com>
2025-02-03 06:35:57 +0530

    many changes

--- Portfolio_website/wsgi.py
+++ Portfolio_website/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for Portfolio_website project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = '/home/Suraj0Singh/My_Portfolio_Website'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'your_project.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Portfolio_website.settings")
-
 application = get_wsgi_application()
